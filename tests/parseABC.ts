ae7b04dbcc3972d4167c12332a9f9f6bffde49b2
K.L. Λ <k.l.lambda@gmail.com>
2025-04-24 16:49:44 +0800

    parseABC.ts: added argv of input filename.

--- tests/parseABC.ts
+++ tests/parseABC.ts
@@ -1,16 +1,20 @@
 
 import fs from "fs";
+import path from "path";
 import YAML from "yaml";
 
 import {parseCode} from "../source/abc/parser";
 
 
 
-const main = async () => {
-	const sourceText = fs.readFileSync("./tests/assets/simple.abc").toString();
+const main = async (abc_path: string): Promise<void> => {
+	const sourceText = fs.readFileSync(abc_path).toString();
 	const raw = await parseCode(sourceText);
 	console.log("result:", raw);
+
+	const stem = path.basename(abc_path);
+	fs.writeFileSync(path.join("./tests/assets/", stem.substring(0, stem.length - 4) + ".local.yaml"), YAML.stringify(raw));
 };
 
 
-main();
+main(process.argv[2] || "./tests/assets/simple.abc");
